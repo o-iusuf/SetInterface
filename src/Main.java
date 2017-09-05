f10c6d7664f223cf5f0368b724fba2a618a9287f
o-iusuf <31664756+o-iusuf@users.noreply.github.com>
2017-09-05 21:56:19 +0300

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -5,23 +5,26 @@ import java.util.TreeSet;
 public class Main {
 
     public static void main(String[] args) {
-        Set<String> s = new TreeSet<>();
+        Set<String> allWords = new TreeSet<>();
 
+        //The TreeSet allWords will contain only one occurence of each word given as input
         for (String a : args) {
             s.add(a);
         }
 
-        System.out.println(s.size() + " distinct words: " + s);
+        System.out.println(allWords.size() + " distinct words: " + allWords);
 
         Set<String> uniques = new HashSet<>();
         Set<String> dups = new HashSet<>();
 
+        //For each word, try to add it to uniques; if it already exists, add it to dups
         for (String a : args) {
             if (!uniques.add(a)) {
                 dups.add(a);
             }
         }
 
+        //Remove all words from uniques which are also found in dups, thus retaining words with only one occurence
         uniques.removeAll(dups);
 
         System.out.println("Unique words: " + uniques);
